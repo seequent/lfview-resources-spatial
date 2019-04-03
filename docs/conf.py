657a6122e2ececab1eb94328760c029ebde45586
Franklin Koch <franklin.koch@seequent.com>
2019-04-03 14:38:34 -0600

    Update conf.py for py3 compatibility

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ try:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-except Exception, e:
+except:
     html_theme = 'default'
 # html_theme = 'alabaster'
 
