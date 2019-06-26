4f17088c3b41e93926bbb085135652ef0155d18a
Yunus Can Emre <yunus.emre@seequent.com>
2019-06-26 14:15:16 -0600

    Bump version: 0.0.4b0 → 0.0.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open('README.rst') as f:
 
 setuptools.setup(
     name='lfview-resources-spatial',
-    version='0.0.4b0',
+    version='0.0.4',
     packages=setuptools.find_packages(exclude=('tests',)),
     install_requires=[
         'properties[full]>=0.5.6',
